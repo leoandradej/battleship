3ee33a34b1b48abb63c4558b815a863ac9d2dc57
Leonardo Andrade <leo_andrade_@hotmail.com>
2024-05-13 23:31:08 -0300

    Landing page correction

--- src/landingPage.js
+++ src/landingPage.js
@@ -1,17 +1,21 @@
 const Gameboard = require('./gameBoard');
 
-const playerGameboard = new Gameboard('player', 10);
-const computerGameboard = new Gameboard('computer', 10);
+const initializePage = () => {
+    const playerGameboard = new Gameboard('player', 10);
+    const computerGameboard = new Gameboard('computer', 10);
 
-const gameboardsContainer = document.querySelector('[data-gameboards-container]');
+    const gameboardsContainer = document.querySelector('[data-gameboards-container]');
 
-gameboardsContainer.append(playerGameboard.createGameboard);
-gameboardsContainer.append(computerGameboard.createGameboard);
+    gameboardsContainer.append(playerGameboard.createGameboard);
+    gameboardsContainer.append(computerGameboard.createGameboard);
 
-const rotateBtn = document.querySelector('.rotate-ship');
+    const rotateBtn = document.querySelector('.rotate-ship');
 
-rotateBtn.addEventListener('click', playerGameboard.flip);
+    rotateBtn.addEventListener('click', playerGameboard.flip);
 
-const startBtn = document.querySelector('.start-btn');
+    const startBtn = document.querySelector('.start-btn');
 
-startBtn.addEventListener('click', playerGameboard.startGame);
+    startBtn.addEventListener('click', playerGameboard.startGame);
+}
+
+export default initializePage;
